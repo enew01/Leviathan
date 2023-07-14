2a1b387e390ca9699ccf4ab27b5c24f9046fcb66
Evan N <enew@es99.agency>
2023-07-14 10:47:37 -0500

    add API files

---
+++ pages/api/army-data.js
@@ -0,0 +1,20 @@
+import path from "path";
+import fs from "fs";
+
+//file name should be Name__Type__(Front||Back).filetype
+//Name = "Primaris+Captain" etc
+export default function handler(req, res) {
+    const { query, method } = req;
+    if (method !== "GET" || !query.name) {
+        res.status(422).json({ status: "failed", message: "Please use correct method and query param." });
+        return;
+    }
+    const armyCardDir = path.join(process.cwd(), `public/cards/${query.name}`);
+    if (!fs.existsSync(armyCardDir)) {
+        res.status(433).json({ status: "failed", message: "please indicate correct directory" });
+    }
+    const armyFileList = fs.readdirSync(armyCardDir);
+    const structuredArmyList = {};
+    armyFileList.foreach((army) => {});
+    res.status(200).json({ status: "success", data: armyFileList });
+}
