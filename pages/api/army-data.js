e62849ebee6d2fd84e80ca7da9f5c0a2ac56ab6f
Evan N <enew@es99.agency>
2023-07-14 13:06:40 -0500

    api update

--- pages/api/army-data.js
+++ pages/api/army-data.js
@@ -10,12 +10,41 @@ export default function handler(req, res) {
         return;
     }
     const armyCardDir = path.join(process.cwd(), `public/cards/${query.name}`);
-    if (!fs.existsSync(armyCardDir)) {
+    const relativeDir = `public/cards/${query.name}`;
+    if (!fs.existsSync(relativeDir)) {
         res.status(400).json({ status: "failed", message: "please indicate correct directory" });
         return;
     }
-    const armyFileList = fs.readdirSync(armyCardDir);
-    const structuredArmyList = {};
-    armyFileList.foreach((army) => {});
-    res.status(200).json({ status: "success", data: armyFileList });
+    const armyFileList = fs.readdirSync(relativeDir);
+    const armyObj = {};
+    for (const army of armyFileList) {
+        const fileType = path.extname(army);
+        const [armyName, armyType, imgDirection] = army.split("__");
+        const fixedImgDir = imgDirection?.replace(fileType, "");
+        if (!armyName || !armyType || (fixedImgDir !== "Front" && fixedImgDir !== "Back")) continue;
+        let dataToEnter =
+            fixedImgDir === "Front"
+                ? {
+                      frontImg: `/cards/${query.name}/${army}`,
+                  }
+                : {
+                      backImg: `/cards/${query.name}/${army}`,
+                  };
+        if (armyObj[armyName]) {
+            armyObj[armyName] = {
+                ...armyObj[armyName],
+                ...dataToEnter,
+            };
+        } else {
+            armyObj[armyName] = {
+                name: armyName.replace("+", " "),
+                type: armyType,
+                ...dataToEnter,
+            };
+        }
+    }
+    for (const key of Object.keys(armyObj)) {
+        console.log(key);
+    }
+    res.status(200).json({ status: "success", data: Object.values(armyObj) });
 }
