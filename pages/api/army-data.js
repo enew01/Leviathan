2779d60944a25df55a5e48d67b486ccb3b3b96ee
Evan N <enew@es99.agency>
2023-07-14 10:56:43 -0500

    error fix

--- pages/api/army-data.js
+++ pages/api/army-data.js
@@ -6,12 +6,13 @@ import fs from "fs";
 export default function handler(req, res) {
     const { query, method } = req;
     if (method !== "GET" || !query.name) {
-        res.status(422).json({ status: "failed", message: "Please use correct method and query param." });
+        res.status(400).json({ status: "failed", message: "Please use correct method and query param." });
         return;
     }
     const armyCardDir = path.join(process.cwd(), `public/cards/${query.name}`);
     if (!fs.existsSync(armyCardDir)) {
-        res.status(433).json({ status: "failed", message: "please indicate correct directory" });
+        res.status(400).json({ status: "failed", message: "please indicate correct directory" });
+        return;
     }
     const armyFileList = fs.readdirSync(armyCardDir);
     const structuredArmyList = {};
